7fc98708ebeb8f98f26831480beffa91cadea0e0
Parisan <parisan sifizadeh>
2025-06-10 16:29:20 +0330

    commiting side efecte

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 //import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Render from "./Render.Jsx";
+//import Render from "./Render.Jsx";
+import Side from "./Side.Jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Render />
+    <Side />
   </React.StrictMode>
 );
 
